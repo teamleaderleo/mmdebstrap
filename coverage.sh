aae47da9ab4a5bc2ae285e990c286bb36a934a2c
Johannes Schauer Marin Rodrigues <josch@mister-muffin.de>
2021-08-16 23:14:18 +0200

    coverage.sh: fix test that was wrongly installing outside the chroot and download-only

--- coverage.sh
+++ coverage.sh
@@ -2893,7 +2893,10 @@ cat << END > shared/test.sh
 #!/bin/sh
 set -eu
 export LC_ALL=C.UTF-8
-$CMD --mode=$defaultmode --variant=essential --include=apt --setup-hook="apt-get update" --setup-hook="apt-get --yes -oApt::Get::Download-Only=true install apt" $DEFAULT_DIST /tmp/debian-chroot.tar $mirror
+$CMD --mode=$defaultmode --variant=essential --include=apt \
+	--essential-hook='apt-get -o Dir="\$1" -oDir::Etc::TrustedParts=/etc/apt/trusted.gpg.d -oAcquire::Languages=none update' \
+	--essential-hook='apt-get --yes install --no-install-recommends -o Dir="\$1" -oDPkg::Chroot-Directory="\$1" apt' \
+	$DEFAULT_DIST /tmp/debian-chroot.tar $mirror
 tar -tf /tmp/debian-chroot.tar | sort | grep -v ./var/lib/apt/extended_states | diff -u tar1.txt -
 rm /tmp/debian-chroot.tar
 END
