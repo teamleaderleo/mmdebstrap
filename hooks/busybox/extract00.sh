8fe4fe3eda94692c3f5080ab543c673658d07ade
Johannes Schauer Marin Rodrigues <josch@mister-muffin.de>
2021-10-08 20:46:55 +0200

    hooks/busybox/extract00.sh: run busybox from an absolute path

--- hooks/busybox/extract00.sh
+++ hooks/busybox/extract00.sh
@@ -4,4 +4,7 @@ set -exu
 
 rootdir="$1"
 
-chroot "$rootdir" busybox --install -s
+# Run busybox using an absolute path so that this script also works in case
+# /proc is not mounted. Busybox uses /proc/self/exe to figure out the path
+# to its executable.
+chroot "$rootdir" /bin/busybox --install -s
