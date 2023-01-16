65ea2edfabd4dc5cfe92ad64eb55939981eff21d
Johannes Schauer Marin Rodrigues <josch@mister-muffin.de>
2023-01-16 07:20:12 +0100

    hooks/no-merged-usr/essential00.sh: symlink to ../merged-usr/essential00.sh
    Both hooks need to install the real usr-is-merged package after having
    installed a dummy in the setup hook even if it's for different reasons.

--- hooks/no-merged-usr/essential00.sh
+++ hooks/no-merged-usr/essential00.sh
@@ -1,15 +1,1 @@
-#!/bin/sh
-
-set -eu
-
-if [ "${MMDEBSTRAP_VERBOSITY:-1}" -ge 3 ]; then
-        set -x
-fi
-
-TARGET="$1"
-
-APT_CONFIG=$MMDEBSTRAP_APT_CONFIG apt-get --yes install -oDPkg::Chroot-Directory="$TARGET" usr-is-merged
-
-chroot "$TARGET" dpkg-query --showformat '${db:Status-Status}\n' --show usr-is-merged | grep -q '^installed$'
-chroot "$TARGET" dpkg-query --showformat '${Source}\n' --show usr-is-merged | grep -q '^usrmerge$'
-dpkg --compare-versions "1" "lt" "$(chroot "$TARGET" dpkg-query --showformat '${Version}\n' --show usr-is-merged)"
+../merged-usr/essential00.sh
